01204b10a35bd9fd49af9718ece9b4160ad2ec9e
Jose Salvatierra <jslvtr@gmail.com>
2014-01-25 19:42:00 +0000

    First commit

---
+++ GameAssignment/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace GameAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(299, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Turn";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(299, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 10;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(299, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Score";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(299, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 12;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(299, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Play again";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Rules";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(299, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Highscore";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(207, 22);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Enter name for player 1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(51, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "Player 1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(411, 217);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GameAssignment/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameAssignment
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Button[,] buttons = new System.Windows.Forms.Button[3, 3];
+        private string play1, play2;
+
+        public Form1()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new System.Windows.Forms.Button();
+                    buttons[i, j].Size = new System.Drawing.Size(75, 50);
+                    buttons[i, j].Location = new System.Drawing.Point(17 + 82 * i, 13 + 55 * j);
+                    buttons[i, j].Font = new System.Drawing.Font("Trebuchet MS", 14.25F, FontStyle.Bold);
+                    buttons[i, j].Click += new System.EventHandler(ClickButton);
+                }
+            }
+            InitializeComponent();
+        }
+
+        public void CreateButtons()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Controls.Add(buttons[i, j]);
+                }
+            }
+            label2.Text = play1;
+            label4.Text = "0 : 0";
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            // We have clicked on the "Rules" button
+            MessageBox.Show("Click on a button if it is your turn. Player 1 = 'X', Player 2 = 'O'.\nHighscores get saved when you reset the score."); 
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            // We have clicked on the "Play again" button
+            // If the board is totally empty, we assume user wants a score reset
+            bool scoreReset = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if(buttons[i, j].Text != "") scoreReset = false;
+                }
+            }
+            if (!scoreReset)
+            {
+                reset();
+            }
+            else
+            {
+                label4.Text = "0 : 0";
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            // We have clicked on Highscore
+
+        }
+
+        private void reset()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                }
+            }
+        }
+
+        public void ClickButton(Object sender, System.EventArgs e)
+        {
+            String character = "X";
+            if (label2.Text == play2)
+            {
+                character = "O";
+            }
+            label2.Text = (label2.Text == play1 ? play2 : play1);
+            Button btn = (Button)sender;
+            btn.Text = character;
+            checkWin();
+        }
+
+        private void checkWin()
+        {
+            int lateral = 0;
+            Button lastLateral = null;
+            for (int i = 0; i < 3; i++)
+            {
+                lateral = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (lastLateral != null)
+                    {
+                        if (buttons[i, j].Text == lastLateral.Text && buttons[i, j].Text != "")
+                        {
+                            lateral++;
+                        }
+                    }
+                    else
+                    {
+                        lateral = 0;
+                    }
+                    lastLateral = buttons[i, j];
+                    if (buttons[0, j].Text == buttons[1, j].Text && buttons[1, j].Text == buttons[2, j].Text && buttons[0, j].Text != "")
+                    {
+                        if (label2.Text == play1)
+                        {
+                            MessageBox.Show(play2 + " wins laterally at " + i+1 + "!");
+                            increasePlayerScore(2);
+                        }
+                        else
+                        {
+                            MessageBox.Show(play1 + " wins laterally at " + i+1 + "!");
+                            increasePlayerScore(1);
+                        }
+                        reset();
+                        break;
+                    }
+                }
+                if (lateral == 2)
+                {
+                    if (label2.Text == play1)
+                    {
+                        MessageBox.Show(play2 + " wins vertically at " + i+1 + "!");
+                        increasePlayerScore(2);
+                    }
+                    else
+                    {
+                        MessageBox.Show(play1 + " wins vertically at " + i+1 + "!");
+                        increasePlayerScore(1);
+                    }
+                    
+                    reset();
+                    break;
+                }
+
+            }
+
+            // Now check the two vertical win situations
+            if ((buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text && buttons[0, 0].Text != "") ||
+                buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text && buttons[2, 0].Text != "")
+            {
+                if (label2.Text == play1)
+                {
+                    MessageBox.Show(play2 + " wins diagonally!");
+                    increasePlayerScore(2);
+                }
+                else
+                {
+                    MessageBox.Show(play1 + " wins diagonally!");
+                    increasePlayerScore(1);
+                }
+                reset();
+            }
+
+        }
+
+        private void increasePlayerScore(int player)
+        {
+            string score = label4.Text;
+            if (player == 1)
+            {
+                string p1 = score.Split(':')[0];
+                int player1 = int.Parse(p1);
+                player1++;
+                label4.Text = player1 + ":" + score.Split(':')[1];
+            }
+            else
+            {
+                string p2 = score.Split(':')[1];
+                int player2 = int.Parse(p2);
+                player2++;
+                label4.Text = score.Split(':')[0] + ":" + player2;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label8.Text.Contains("1"))
+            {
+                play1 = textBox1.Text;
+                label8.Text = "Enter name for player 2";
+                textBox1.Text = "Player 2";
+                textBox1.SelectAll();
+            }
+            else
+            {
+                play2 = textBox1.Text;
+                label8.Hide();
+                button1.Hide();
+                textBox1.Hide();
+                CreateButtons();
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
