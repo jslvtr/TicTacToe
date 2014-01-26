86e209d918274eaf0d26cfa1616dd20e54a1dfb8
Jose Salvatierra <jslvtr@gmail.com>
2014-01-26 00:14:47 +0000

    Created High Score form and created new class to store High Scores: Score.cs.

---
+++ GameAssignment/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameAssignment
+{
+    public partial class Form2 : Form
+    {
+
+        private Score[] scores;
+
+        public Form2()
+        {
+            scores = new Score[15];
+            InitializeComponent();
+        }
+
+        public void addScore(string player1, string player2, string score)
+        {
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] == null)
+                {
+                    scores[i] = new Score(player1, player2, score);
+                    return;
+                }
+            }
+            scores[scores.Length] = new Score(player1, player2, score);
+
+            //IEnumerable<Score> qry = scores;
+            //if (sortCols.Length > 0)
+            //{
+            //    IOrderedEnumerable<int[]> sorted =
+            //        qry.OrderBy(row => row[sortCols[0]]);
+            //    for (int i = 1; i < sortCols.Length; i++)
+            //    {
+            //        int col = sortCols[i]; // for capture
+            //        sorted = sorted.ThenBy(row => row[col]);
+            //    }
+            //    qry = sorted;
+            //}
+
+        }
+
+        private void shiftArrayUp(Object[] arr)
+        {
+            Object[] output = new Object[arr.Length];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                output[i - 1] = arr[i];
+            }
+            output[arr.Length] = null;
+            arr = output;
+        }
+    }
+}
---
+++ GameAssignment/Score.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAssignment
+{
+    class Score
+    {
+        private string player1, player2;
+        private string score;
+        private int diff;
+
+        public Score(string p1, string p2, string sc)
+        {
+            player1 = p1;
+            player2 = p2;
+            score = sc;
+            diff = Math.Abs(int.Parse(sc.Split(':')[0]) - int.Parse(sc.Split(':')[1]));
+        }
+
+        public string getPlayer1()
+        {
+            return this.player1;
+        }
+
+        public string getPlayer2()
+        {
+            return this.player2;
+        }
+
+        public string getScore()
+        {
+            return this.score;
+        }
+
+        public int getDiff()
+        {
+            return this.diff;
+        }
+    }
+}
